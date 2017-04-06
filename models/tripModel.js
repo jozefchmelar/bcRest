84c04198c3180646b8dc41456657763db8b6c5cd
jozefchmelar <jozefchmelar@gmail.com>
2017-04-06 14:18:33 +0200

    fix

---
+++ models/tripModel.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var employeeSchema = require('./employeeModel');
+var autoIncrement = require('mongoose-auto-increment');
+
+//var projectSchema = require('./projectModel');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var businessTripSchema = new Schema({  
+
+    employees: [{type: Number , ref:'Employee',required: true}],
+    car:  {type: String ,required: true } ,
+    reason:{type:String ,required: true }
+},
+    {
+        versionKey: false // disables __v  in schema
+    });
+
+ 
+
+var businessTripModel = mongoose.model('Trip', businessTripSchema);
+module.exports = businessTripModel;
